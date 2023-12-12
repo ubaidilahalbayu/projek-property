fd0885c9c3d6304e10532caecef2962676dc79e8
codeinstant <codeinstan@gmail.com>
2023-12-12 22:12:04 +0700

    update3

---
+++ db/tb_sales.sql
@@ -0,0 +1,57 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost
+-- Generation Time: Dec 12, 2023 at 10:10 PM
+-- Server version: 8.0.35-0ubuntu0.22.04.1
+-- PHP Version: 7.4.33
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `db_property`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tb_sales`
+--
+
+CREATE TABLE `tb_sales` (
+  `nik_sales` varchar(20) NOT NULL,
+  `nama_sales` varchar(100) NOT NULL,
+  `alamat_sales` text NOT NULL,
+  `no_hp_sales` varchar(15) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `tb_sales`
+--
+
+INSERT INTO `tb_sales` (`nik_sales`, `nama_sales`, `alamat_sales`, `no_hp_sales`) VALUES
+('090218144', 'Ubei', 'Belitang', '+62582371210055');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `tb_sales`
+--
+ALTER TABLE `tb_sales`
+  ADD PRIMARY KEY (`nik_sales`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
